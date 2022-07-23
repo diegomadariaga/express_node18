b5f42f270c91d9f3dae7308eec3dc205d6a7e8f7
diego madariaga <diegonicolasmadariaga@gmail.com>
2022-07-22 22:54:02 -0400

    create service

---
+++ src/v1/routes/workoutRoutes.js
@@ -0,0 +1,12 @@
+//@ts-check
+const express = require("express");
+const router = express.Router();
+const workoutsController = require("../../controller/workoutsController");
+
+router.get("/", workoutsController.getAllWorkouts);
+router.get("/:id", workoutsController.getWorkoutById);
+router.post("/", workoutsController.createWorkout);
+router.put("/:id", workoutsController.updateWorkout);
+router.delete("/:id", workoutsController.deleteWorkout);
+
+module.exports = router;
