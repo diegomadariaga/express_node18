18c10015d9105011328276814fadc997a40efae6
diego madariaga <diegonicolasmadariaga@gmail.com>
2022-07-22 21:36:18 -0400

    reset app file

--- src/app.js
+++ src/app.js
@@ -1,16 +1,1 @@
-import express from 'express';
-
-// Create a new express application instance
-const app = express();
-
-// The port the express app will listen on
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
-
-// get endpoint
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+console.log('Hello World!');
