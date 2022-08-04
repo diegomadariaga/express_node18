3e6478a2d5304cfa76e6fd962eec87cb8fc1f04a
diego madariaga <diegonicolasmadariaga@gmail.com>
2022-08-04 11:11:06 -0400

    ok

--- src/services/workoutsService.js
+++ src/services/workoutsService.js
@@ -1,37 +1,37 @@
 const workouts = [
-  {
-    id: 1,
-    name: 'Bench Press',
-    description: 'Bench Press',
-    muscles: 'Chest',
-    equipment: 'Barbell',
-    image: 'https://www.bodybuilding.com/exercises/exerciseImages/quadriceps-bench-press-1.jpg',
-    video: 'https://www.youtube.com/watch?v=_q-rZQ-_zDU',
-  },
-  {
-    id: 2,
-    name: 'Incline Bench Press',
-    description: 'Incline Bench Press',
-    muscles: 'Chest',
-    equipment: 'Barbell',
-    image: 'https://www.bodybuilding.com/exercises/exerciseImages/quadriceps-bench-press-1.jpg',
-    video: 'https://www.youtube.com/watch?v=_q-rZQ-_zDU',
-  },
-  {
-    id: 3,
-    name: 'Decline Bench Press',
-    description: 'Decline Bench Press',
-    muscles: 'Chest',
-    equipment: 'Barbell',
-    image: 'https://www.bodybuilding.com/exercises/exerciseImages/quadriceps-bench-press-1.jpg',
-    video: 'https://www.youtube.com/watch?v=_q-rZQ-_zDU',
-  },
+    {
+        id: 1,
+        name: "Bench Press",
+        description: "Bench Press",
+        muscles: "Chest",
+        equipment: "Barbell",
+        image: "https://www.bodybuilding.com/exercises/exerciseImages/quadriceps-bench-press-1.jpg",
+        video: "https://www.youtube.com/watch?v=_q-rZQ-_zDU",
+    },
+    {
+        id: 2,
+        name: "Incline Bench Press",
+        description: "Incline Bench Press",
+        muscles: "Chest",
+        equipment: "Barbell",
+        image: "https://www.bodybuilding.com/exercises/exerciseImages/quadriceps-bench-press-1.jpg",
+        video: "https://www.youtube.com/watch?v=_q-rZQ-_zDU",
+    },
+    {
+        id: 3,
+        name: "Decline Bench Press",
+        description: "Decline Bench Press",
+        muscles: "Chest",
+        equipment: "Barbell",
+        image: "https://www.bodybuilding.com/exercises/exerciseImages/quadriceps-bench-press-1.jpg",
+        video: "https://www.youtube.com/watch?v=_q-rZQ-_zDU",
+    },
 ];
 function getAllWorkouts() {
-  return workouts;
+    return workouts;
 }
 function getWorkoutById(id) {
-  return workouts.find((workout) => workout.id === id);
+    return workouts.find((workout) => workout.id === id);
 }
 /**
  * Create a workout by adding it to the workouts array and returning the workout.
@@ -39,30 +39,30 @@ function getWorkoutById(id) {
  * @returns The workout object is being returned.
  */
 function createWorkout(workout) {
-  workout.id = workouts.length + 1;
-  workouts.push(workout);
-  return workout;
+    workout.id = workouts.length + 1;
+    workouts.push(workout);
+    return workout;
 }
 function updateWorkout(id, workout) {
-  const index = workouts.findIndex((w) => w.id === id);
-  if (index !== -1) {
-    workouts[index] = workout;
-    return workout;
-  }
-  return null;
+    const index = workouts.findIndex((w) => w.id === id);
+    if (index !== -1) {
+        workouts[index] = workout;
+        return workout;
+    }
+    return null;
 }
 function deleteWorkout(id) {
-  const index = workouts.findIndex((w) => w.id === id);
-  if (index !== -1) {
-    workouts.splice(index, 1);
-    return true;
-  }
-  return false;
+    const index = workouts.findIndex((w) => w.id === id);
+    if (index !== -1) {
+        workouts.splice(index, 1);
+        return true;
+    }
+    return false;
 }
 module.exports = {
-  getAllWorkouts,
-  getWorkoutById,
-  createWorkout,
-  updateWorkout,
-  deleteWorkout,
+    getAllWorkouts,
+    getWorkoutById,
+    createWorkout,
+    updateWorkout,
+    deleteWorkout,
 };
